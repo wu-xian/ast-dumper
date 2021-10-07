ce9d81c9f2d63aa212d831566f8c6683db3e0036
Xan <270084782@qq.com>
2021-10-07 18:34:53 +0800

    fix:

--- main.go
+++ main.go
@@ -10,16 +10,10 @@ import (
 func main() {
 	if len(os.Args) != 2 {
 		fmt.Println("Usage:")
-		fmt.Println("\tastdump [filename]")
+		fmt.Println("\tast-dumper [filename]")
 		os.Exit(1)
 	}
 	fname := os.Args[1]
-	file, err := os.Open(fname)
-	if err != nil {
-		fmt.Println("cannot open file: %v", err)
-		os.Exit(1)
-	}
-	defer file.Close()
 	errh := syntax.ErrorHandler(
 		func(err error) {
 			fmt.Println(err)
